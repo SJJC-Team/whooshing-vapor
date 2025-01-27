254446e28c66bc41e7bc464b62b92e390039c983
ChenLin Wang <wangchenlin2001@icloud.com>
2025-01-27 09:18:33 +0800

    Update HTTPServerIOHandler.swift

--- Sources/Vapor/HTTP/Server/HTTPServerIOHandler.swift
+++ Sources/Vapor/HTTP/Server/HTTPServerIOHandler.swift
@@ -27,31 +27,31 @@ public protocol HTTPIOHandler: Sendable {
     /// - 参数：
     ///     - request: 从客户端发来的原始的未解密的请求数据 Data
     ///     - context: 该请求连线的上下文
-    /// - 返回：解包过后的请求数据 Data
-    func input(request: Data, context: ChannelHandlerContext) throws -> Data
+    /// - 返回：解包过后的请求数据 Data，若返回 nil 则意味着捕获该请求，而不再进行后续处理
+    func input(request: Data, context: ChannelHandlerContext) throws -> Data?
     /// 当该服务器对客户端做出响应时，包装将要做出的响应。通常是进行加密操作，以保护响应不被窃取
     ///
     /// - 参数：
     ///     - request: 服务器将要发送给客户端的响应数据 Data
     ///     - context: 该请求连线的上下文，可以通过其获取对应的 Info，见下一个参数
     ///     - info: 该连线的附加信息，包括是否为 WebSocket，以及当前请求的 ID
-    /// - 返回：处理过后的响应数据 Data
-    func output(response: Data, context: ChannelHandlerContext, info: ChannelInfo) throws -> Data
+    /// - 返回：处理过后的响应数据 Data，若返回 nil 则意味着捕获该请求，而不再进行后续处理
+    func output(response: Data, context: ChannelHandlerContext, info: ChannelInfo) throws -> Data?
     /// 当接受到请求时，解析传来的请求。通常是进行解密操作，以将密文转为下一步可解析的 HTTP 报文。免去 ByteBuffer 与 Data 互转的步骤，直接操作 ByteBuffer，更加轻量
     ///
     /// - 参数：
     ///     - request: 从客户端发来的原始的未解密的请求数据 ByteBuffer
     ///     - context: 该请求连线的上下文
-    /// - 返回：解包过后的请求数据 ByteBuffer
-    func input(request: ByteBuffer, context: ChannelHandlerContext) throws -> ByteBuffer
+    /// - 返回：解包过后的请求数据 ByteBuffer，若返回 nil 则意味着捕获该请求，而不再进行后续处理
+    func input(request: ByteBuffer, context: ChannelHandlerContext) throws -> ByteBuffer?
     /// 当该服务器对客户端做出响应时，包装将要做出的响应。通常是进行加密操作，以保护响应不被窃取。免去 ByteBuffer 与 Data 互转的步骤，直接操作 ByteBuffer，更加轻量
     ///
     /// - 参数：
     ///     - request: 服务器将要发送给客户端的响应数据 ByteBuffer
     ///     - context: 该请求连线的上下文，可以通过其获取对应的 Info，见下一个参数
     ///     - info: 该连线的附加信息，包括是否为 WebSocket，以及当前请求的 ID
-    /// - 返回：处理过后的响应数据 ByteBuffer
-    func output(response: ByteBuffer, context: ChannelHandlerContext, info: ChannelInfo) throws -> ByteBuffer
+    /// - 返回：处理过后的响应数据 ByteBuffer，若返回 nil 则意味着捕获该请求，而不再进行后续处理
+    func output(response: ByteBuffer, context: ChannelHandlerContext, info: ChannelInfo) throws -> ByteBuffer?
     
     /// 当连线建立后，调用此方法，不提供 Info 参数，因为此时还未初始化该参数。默认不进行任何动作
     func connectionStart(context: ChannelHandlerContext) throws
@@ -63,21 +63,27 @@ public protocol HTTPIOHandler: Sendable {
 public extension HTTPIOHandler {
     
     /// 默认不进行任何处理，直接将 request 作为返回值
-    func input(request: Data, context: ChannelHandlerContext) throws -> Data { request }
+    func input(request: Data, context: ChannelHandlerContext) throws -> Data? { request }
     
     /// 默认不进行任何处理，直接将 response 作为返回值
-    func output(response: Data, context: ChannelHandlerContext, info: ChannelInfo) throws -> Data { response }
+    func output(response: Data, context: ChannelHandlerContext, info: ChannelInfo) throws -> Data? { response }
     
     /// 默认调用 `func input(request: Data, context: ChannelHandlerContext, info: ChannelInfo)` 完成处理
-    func input(request: ByteBuffer, context: ChannelHandlerContext) throws -> ByteBuffer {
+    func input(request: ByteBuffer, context: ChannelHandlerContext) throws -> ByteBuffer? {
         let req = Data(buffer: request)
-        return try dataToByteBuffer(data: input(request: req, context: context))
+        if let data = try input(request: req, context: context) {
+            return dataToByteBuffer(data: data)
+        }
+        return nil
     }
     
     /// 默认调用 `func output(response: Data, context: ChannelHandlerContext, info: ChannelInfo)` 完成处理
-    func output(response: ByteBuffer, context: ChannelHandlerContext, info: ChannelInfo) throws -> ByteBuffer {
+    func output(response: ByteBuffer, context: ChannelHandlerContext, info: ChannelInfo) throws -> ByteBuffer? {
         let res = Data(buffer: response)
-        return try dataToByteBuffer(data: output(response: res, context: context, info: info))
+        if let data = try output(response: res, context: context, info: info) {
+            return dataToByteBuffer(data: data)
+        }
+        return nil
     }
     
     func connectionStart(context: ChannelHandlerContext) throws {}
@@ -112,8 +118,9 @@ final internal class CustomCryptoIOHandler: ChannelDuplexHandler {
         let buffer = self.unwrapInboundIn(data)
         if let ioHandler = self.ioHandler {
             do {
-                let req = try ioHandler.input(request: buffer, context: context)
-                context.fireChannelRead(self.wrapOutboundOut(req))
+                if let req = try ioHandler.input(request: buffer, context: context) {
+                    context.fireChannelRead(self.wrapOutboundOut(req))
+                }
             } catch let err {
                 errorCaught(context: context, label: "Input", error: err)
             }
@@ -139,8 +146,9 @@ final internal class CustomCryptoIOHandler: ChannelDuplexHandler {
                 }
                 i = 0
                 do {
-                    let res = try ioHandler.output(response: res, context: context, info: channelInfo)
-                    context.writeAndFlush(self.wrapOutboundOut(res), promise: promise)
+                    if let res = try ioHandler.output(response: res, context: context, info: channelInfo) {
+                        context.writeAndFlush(self.wrapOutboundOut(res), promise: promise)
+                    }
                 } catch let err {
                     errorCaught(context: context, label: "Output", error: err)
                 }
